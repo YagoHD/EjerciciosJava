f56c86b7c998c4db13e0c16c590217980b117ce6
Yago Ramos Bermúdez <yagorb@hotmail.com>
2022-06-14 00:46:18 +0200

    Add: EJERCICIOS

---
+++ JavaEjercicio18/src/NumerosAleatorios.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Introduce el primer numero: ");
+        int num1 = sc.nextInt();
+        System.out.println("Introduce el segunod numero: ");
+        int num2 = sc.nextInt();
+        
+        for (int i=0;i<10;i++){
+            //Generamos un numero aleatorio
+            int numAleatorio=(int)(Math.random()*(num1-num2)+num2);
+            System.out.println(numAleatorio);
+        }
+        
+    }
+}
